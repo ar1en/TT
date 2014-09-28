8e36b7e9a98bdc990a34740592a5c901554709de
ar1en <ar1en@hotmail.com>
2014-09-28 17:25:35 +0400

    4
    4

--- Assets/Scripts/gui.cs
+++ Assets/Scripts/gui.cs
@@ -6,11 +6,6 @@ public class gui : MonoBehaviour
 	void OnGUI()
 	{
 		tetrisMain _main = this.GetComponent<tetrisMain>();
-<<<<<<< HEAD
 		GUI.Label (new Rect (Screen.width-Screen.width/4, 25, 100, 30), "Score: "+ _main.score + "ololo");
-=======
-		GUI.Label (new Rect (Screen.width-Screen.width/4, 25, 100, 30), "Score: "+ _main.score);
-		Debug.Log("1");
->>>>>>> origin/master
 	}
 }
