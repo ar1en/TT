49417576554712695dae49fc60425724a2cd9614
ar1en <ar1en@hotmail.com>
2014-09-30 14:05:59 +0400

    pause aded

--- Assets/Scripts/gui.cs
+++ Assets/Scripts/gui.cs
@@ -7,5 +7,18 @@ public class gui : MonoBehaviour
 	{
 		tetrisMain _main = this.GetComponent<tetrisMain>();
 		GUI.Label (new Rect (Screen.width-Screen.width/4, 25, 100, 30), "Score: "+ _main.score);
+		if (Time.timeScale == 0)
+			GUI.Label (new Rect (Screen.width/2, Screen.height/2, 100, 30), "Pause");
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) 
+		{
+			if (Time.timeScale == 1)
+				Time.timeScale = 0;
+			else
+				Time.timeScale = 1;
+		}
 	}
 }
