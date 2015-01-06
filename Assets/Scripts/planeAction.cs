c29f03a27b919baeb7b79938227d05da542a7b9a
ar1en <ar1en@hotmail.com>
2015-01-06 23:58:25 +0300

    Plane animation added

---
+++ Assets/Scripts/planeAction.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class planeAction : MonoBehaviour 
+{
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+		transform.position = new Vector3 (transform.position.x, transform.position.y + 1 * Time.deltaTime, transform.position.z);
+	}
+}
